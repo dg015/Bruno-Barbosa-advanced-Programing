66049eaa5942a95e44e13bd8ef7b70c395c2510b
dg015 <bruno.barbosa8@yahoo.com>
2025-09-07 21:52:12 -0400

    Mouse movement detection added

--- Bruno Barbosa advanced Programing/Assets/Script/AttackDirectionDetection.cs
+++ Bruno Barbosa advanced Programing/Assets/Script/AttackDirectionDetection.cs
@@ -6,34 +6,52 @@ using UnityEngine.UI;
 public class AttackDirectionDetection : MonoBehaviour
 {
     [SerializeField] private Image[] image;
+    [SerializeField] private Vector2 oldMousePosition;
+    [SerializeField] private Vector2 MousePosition; 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+       
+        oldMousePosition = MousePosition;
     }
 
     // Update is called once per frame
     void Update()
     {
+        MousePosition = new Vector2(Input.mousePosition.x / Screen.width, Input.mousePosition.y / Screen.height);
         getMouseLocation();
     }
 
     private void getMouseLocation()
     {
-        //first get the new mouse location
-        //set the center to 0.5( easier for math I think)
-        //later every frame set the past position as the last location
+        // -------------------------Process to follow----------------------------
+        //first get the new mouse location     -> done
+        //set the center to 0.5( easier for math I think)   --> done
+        //later every frame set the past position as the last location     
         //if the mouse has moved get location between past location and new location
         //compare location to angles to get the named direction ( up, left, right, down left, down right)
         //Paint the equivalent UI element red and set the previous one as white
+        //--------------------------End of idea process --------------------------
 
+
+        // set the starting mouse location to 0 (center)
         Vector2 startingMouseLocation = new Vector2 (0,0);
+        //get the mouse lcoation
         Vector2 mouseLocation = Input.mousePosition;
-        Vector2 normalizedPosition = new Vector2(mouseLocation.x/Screen.width, mouseLocation.y/Screen.height);
+        // get the center to 0.5 for easier math
 
-
-        Debug.Log(normalizedPosition);
+        if (MousePosition != oldMousePosition)
+        {
+            oldMousePosition = MousePosition;
+            Debug.Log("mouse Moved");
+        }
+        else
+        {
+            Debug.Log("mouse stayed");
+            oldMousePosition = MousePosition;
+        }
+        
     }
 
     private void updateUI()
