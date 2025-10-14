e3b530f65cab628f7ee591cd4721a3884c130096
dg015 <bruno.barbosa8@yahoo.com>
2025-10-14 08:22:52 -0400

    Created function for dodge
    Started to work on fcuntion, currenty only have to check if the key was pressed to start the timer

--- Bruno Barbosa advanced Programing/Assets/Script/PlayerMovement.cs
+++ Bruno Barbosa advanced Programing/Assets/Script/PlayerMovement.cs
@@ -5,6 +5,8 @@ public class PlayerMovement : MonoBehaviour
 {
 
     [SerializeField] private CharacterController characterController;
+    [SerializeField] private float dodgeTimer;
+    [SerializeField] private float dodgeTimerMax;
 
     [SerializeField] private float speed = 12;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -33,6 +35,27 @@ public class PlayerMovement : MonoBehaviour
 
 
 
+
+    /// <summary>
+    /// check if the key was pressed
+    /// if the key was pressed run timer for x seconds
+    /// if the timer runs out set the timer to 0 and reset
+    /// if they press the key again in that time period dodge happens
+    /// </summary>
+    private void dodge()
+    {
+        
+        //checking for pressing the key
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            //increase timer
+            dodgeTimer += Time.deltaTime;
+        }
+        if(dodgeTimer > dodgeTimerMax)
+        {
+
+        }
+    }
 
 
 }
