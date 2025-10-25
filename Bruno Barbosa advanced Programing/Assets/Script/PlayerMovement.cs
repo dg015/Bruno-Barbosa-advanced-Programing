8e5bd9c6b45da4476a06906c87f6a1402a9f243d
dg015 <bruno.barbosa8@yahoo.com>
2025-10-25 17:09:34 -0400

    Player is now able to crouch and get back up
    Created a new function using an animation curve to manipulate the player's Y position to make the player model crouch using the previous implemented IK

--- Bruno Barbosa advanced Programing/Assets/Script/PlayerMovement.cs
+++ Bruno Barbosa advanced Programing/Assets/Script/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -9,9 +10,20 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float dodgeTimerMax;
 
     [SerializeField] private float speed = 12;
+
+    [SerializeField] private float dodgeDuration;
+
+    [SerializeField] private float dodgeDurationTimer;
+    [SerializeField] private bool dodging;
+    [SerializeField] private float originalYValue;
+    [SerializeField] private AnimationCurve dodgeCurve;
+
+
+    private float crouchHeight;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        originalYValue = transform.position.y;
 
     }
 
@@ -21,19 +33,46 @@ public class PlayerMovement : MonoBehaviour
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
+        float startingY = transform.position.y;
+        crouchHeight = startingY - crouchHeight;
+
         Vector3 move = transform.right * x + transform.forward * z;
 
-        characterController.Move(move * speed * Time.deltaTime); 
-
+        characterController.Move(move * speed * Time.deltaTime);
+        Checkdodge();
+        //for testing
+        if(Input.GetKeyDown(KeyCode.LeftShift) && !dodging)
+        {
+            dodgeDurationTimer = 0f;
+            dodging = true;
+        }
+        if(dodging == true)
+        {
+            crouching();
+        }
     }
 
-    private void FixedUpdate()
+
+    private void crouching( )
     {
+        Debug.Log("dodging");
 
+        dodgeDurationTimer += Time.deltaTime;
+        float t = Mathf.Clamp01(dodgeDurationTimer/dodgeDuration);
+
+        float newY = dodgeCurve.Evaluate(t);
+
+        transform.position = new Vector3 (transform.position.x, originalYValue - newY, transform.position.z);
+
+        if (t >= 1f)
+        {
+            dodging = false;
+        }
     }
 
 
 
+ 
 
 
     /// <summary>
@@ -42,7 +81,7 @@ public class PlayerMovement : MonoBehaviour
     /// if the timer runs out set the timer to 0 and reset
     /// if they press the key again in that time period dodge happens
     /// </summary>
-    private void dodge()
+    private void Checkdodge()
     {
         
         //checking for pressing the key
