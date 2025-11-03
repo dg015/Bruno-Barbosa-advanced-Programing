5bef00b7a0fae75338b1c984c34e14cf3eecb504
dg015 <bruno.barbosa8@yahoo.com>
2025-11-03 16:10:18 -0500

    Finished implement dodge - added force after dodge
    - Created function that checks if the player has double tapped the keys within time frame to initate dodge
    - changed dodge option so that the player is able to get a speed boost after doding based onthe last key pressed by applying a force and changing the maximum speed
    -Fixed jittering bug when crouching by placing IK management inside of fixed update so it matches the same update frame as the rigid body

--- Bruno Barbosa advanced Programing/Assets/Script/RigidBodyCharacterController.cs
+++ Bruno Barbosa advanced Programing/Assets/Script/RigidBodyCharacterController.cs
@@ -1,9 +1,11 @@
+using System;
 using Unity.VisualScripting;
 using UnityEngine;
 
 public class RigidBodyCharacterController : MonoBehaviour
 {
     [SerializeField] private Rigidbody rb;
+    [SerializeField] private CapsuleCollider playerCollider;
 
     [Header("Speed")]
     //decelerating using drag
@@ -14,7 +16,9 @@ public class RigidBodyCharacterController : MonoBehaviour
 
     //acelerating
     [SerializeField] private float acceleration;
-    [SerializeField] private float maxSpeed;
+    [SerializeField] private float currentMaxSpeed;
+    [SerializeField] private float defaultMaxSpeed;
+    [SerializeField] private float boostedMaxSpeed;
     //[SerializeField] private AnimationCurve maxSpeedCurve;
     //[SerializeField] private float speedBoostDuration;
     //[SerializeField] private float speedBoostStrengh;
@@ -22,7 +26,7 @@ public class RigidBodyCharacterController : MonoBehaviour
     
     //speed boost while starting walking
 
-    [SerializeField] private float currentSpeed;
+     private float currentSpeed;
 
 
     [Header("Inputs")]
@@ -37,13 +41,30 @@ public class RigidBodyCharacterController : MonoBehaviour
     [SerializeField] private LayerMask ground;
     bool grounded;
 
+    [Header("Dodge")]
     Vector3 MoveDirection;
+    //dodging
+    private float dodgeDurationTimer;
+    [SerializeField] private bool dodging;
+    private float originalYValue;
+    [SerializeField] private AnimationCurve dodgeCurve;
+    [SerializeField] private float dodgeDuration;
+    //changing the scale while doding
 
+    [SerializeField] private float defaultColliderHeight;
+    [SerializeField] private float dodgeForce;
+    int keyPresses;
+    float DodgeRunTime = 0;
+    [SerializeField] private KeyCode lastPressedKey;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        currentMaxSpeed = defaultMaxSpeed;
         rb = GetComponent<Rigidbody>();
+        playerCollider = rb.GetComponent<CapsuleCollider>();    
         rb.freezeRotation = true;
+        originalYValue = transform.localPosition.y;
+        defaultColliderHeight = playerCollider.height;
     }
 
     private void getInput()
@@ -53,18 +74,63 @@ public class RigidBodyCharacterController : MonoBehaviour
 
     }
 
+    private void MultipleTapInput(KeyCode key, float maxTimer, int requiredKeyPresses)
+    {
+      
+        if(Input.GetKeyDown(key))
+        {
+            lastPressedKey = key;
+            keyPresses++;
+        }
+        if(keyPresses >0)
+        {
+            DodgeRunTime += Time.deltaTime;
+        }
+        if(keyPresses >= requiredKeyPresses)
+        {
+            //do action
+            //for now i call the action here but later make it so I can just pass it as a method
+            dodging = true;
+            dodgeDurationTimer = 0f;
+            Debug.Log("action Started");
+            keyPresses = 0;
+            DodgeRunTime = 0;
+        }
+        if(DodgeRunTime >= maxTimer)
+        {
+            keyPresses = 0;
+            DodgeRunTime = 0;
+            Debug.Log("time ran out");
+        }
+        
+       
+    }
+
     // Update is called once per frame
     void Update()
     {
+        
         getCameraDireciton();
         getInput();
         isGroundedCheck(grounded, 15f);
         stopGravity(grounded);
-       
+
+        MultipleTapInput(KeyCode.D, 1f, 2);
+        MultipleTapInput(KeyCode.A, 1f, 2);
+
     }
+
     private void FixedUpdate()
     {
         movePlayer();
+
+        if (dodging == true)
+        {
+            crouching();
+        }
+        else if (dodging == false)
+            playerCollider.height = defaultColliderHeight;
+
     }
 
     void getCameraDireciton()
@@ -106,6 +172,41 @@ public class RigidBodyCharacterController : MonoBehaviour
         }
 
     }
+    private void crouching()
+    {
+        currentMaxSpeed = boostedMaxSpeed;
+        //start a timer for the duration of the dodge
+        dodgeDurationTimer += Time.deltaTime;
+        float t = Mathf.Clamp01(dodgeDurationTimer / dodgeDuration);
+
+        //lower the Y value using the curve
+        float newY = dodgeCurve.Evaluate(t);
+        transform.position = new Vector3(transform.position.x, originalYValue - newY, transform.position.z);
+       
+        //reduce player height to not cause clipping with the ground
+        playerCollider.height = defaultColliderHeight - newY;
+
+
+        //applying force
+        if(lastPressedKey == KeyCode.A)
+        {
+            rb.AddForce(- transform.right,ForceMode.Impulse);
+        }
+        if (lastPressedKey == KeyCode.D)
+        {
+            rb.AddForce(transform.right, ForceMode.Impulse);
+        }
+        //when t is bigger then 1 that means the animation is over and the dodge is over
+        if (t >= 1f)
+        {
+            Vector3 afterDodgeSpeed = new Vector3 (0,rb.linearVelocity.y,0);
+            rb.linearVelocity = afterDodgeSpeed;
+            currentMaxSpeed = defaultMaxSpeed;
+
+            dodging = false;
+        }
+    }
+
 
 
     /// <summary>
@@ -124,7 +225,7 @@ public class RigidBodyCharacterController : MonoBehaviour
            // maxSpeed = maxSpeed + maxSpeedBoosted;
             rb.linearDamping = 0;
             currentSpeed += acceleration * Time.deltaTime;
-            currentSpeed = Mathf.Min(currentSpeed, maxSpeed);
+            currentSpeed = Mathf.Min(currentSpeed, currentMaxSpeed);
             rb.AddForce(MoveDirection * currentSpeed, ForceMode.Force);
             
         }
