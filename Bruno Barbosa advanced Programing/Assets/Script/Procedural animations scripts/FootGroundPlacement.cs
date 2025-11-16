b30e4e389e18c67a78f661c2da52aad51f5c1432
dg015 <bruno.barbosa8@yahoo.com>
2025-11-16 10:07:42 -0500

    Player feet now recalculate position based on rotation while staying still
    -Added a new method that checks if the player is still, if still then get the old angle and check the current rotation with the old angle if bigger then the required angle, then recalculate feet

--- Bruno Barbosa advanced Programing/Assets/Script/Procedural animations scripts/FootGroundPlacement.cs
+++ Bruno Barbosa advanced Programing/Assets/Script/Procedural animations scripts/FootGroundPlacement.cs
@@ -1,10 +1,11 @@
+using Unity.Burst.CompilerServices;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Animations.Rigging;
 
 public class FootGroundPlacement : MonoBehaviour
 {
     [Header("Raycast")]
-    [SerializeField] private float YOffset;
     [SerializeField] private Transform playerModel;
     [SerializeField] private float footspacing;
     [SerializeField] private LayerMask groundLayer;
@@ -13,11 +14,11 @@ public class FootGroundPlacement : MonoBehaviour
     [Header("Location")]
     [SerializeField] private float stepDistance;
     [SerializeField] private float velocityPredictionMultiplier = 0.3f;
-
+    private Ray feetRay;
     private Vector3 currentPosition, nextPosition, oldPosition;
 
     private Vector3 lastPosition;
-    [SerializeField] private Vector3 velocity;
+    private Vector3 velocity;
 
 
     [Header("Animation")]
@@ -35,9 +36,11 @@ public class FootGroundPlacement : MonoBehaviour
     [SerializeField] private float hipAnimationModifier;
 
     public bool IsMoving => lerp < 1f;
-
-
-
+    [Header("Reposition feet after rotating")]
+    [SerializeField] private bool newRotation;
+    [SerializeField] private float oldAngle;
+    [SerializeField] private bool recalcualte;
+    [SerializeField] private int angleToChange;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -54,29 +57,32 @@ public class FootGroundPlacement : MonoBehaviour
     {
         moveLeg();
         playerSpeedModifier();
-        hipMovement();
+        hipMovement(hipAnimationModifier);
+        postitioningRotationAdjustament(angleToChange, playerModel);
     }
     private void moveLeg()
     {
         //set the position as the current position so feet i stuck in locaiton
         transform.position = currentPosition;
 
-        Ray ray = new Ray(playerModel.position + (playerModel.right * footspacing) + Vector3.up * 2, Vector3.down);
+        feetRay = new Ray(playerModel.position + (playerModel.right * footspacing) + Vector3.up * 2, Vector3.down);
 
-        if (Physics.Raycast(ray, out RaycastHit hit, 10, groundLayer))
+        if (Physics.Raycast(feetRay, out RaycastHit hit, 10, groundLayer))
         {
             //if the closest location is far enough set it as the new location
-            if (Vector3.Distance(nextPosition, hit.point) >= stepDistance && !other.IsMoving && lerp >= 1f)
+            if (Vector3.Distance(nextPosition, hit.point) >= stepDistance && !other.IsMoving && lerp >= 1f || recalcualte)
             {
+                
                 Debug.Log(hit.collider);
                 lerp = 0f;
 
                 nextPosition = hit.point;
                 nextPosition += velocity * velocityPredictionMultiplier;
                 oldPosition = transform.position;
-                Debug.Log("new location found");
-            }
+                
 
+                //calculatingNextLocaiton(hit);
+            }
         }
         if (lerp <= 1f)
         {
@@ -85,21 +91,65 @@ public class FootGroundPlacement : MonoBehaviour
 
             currentPosition = footPosition;
             lerp += Time.deltaTime * stepSpeed;
+        }     
+    }
+
+    
+    /// <summary>
+    /// get transform and set the angle
+    /// Check if the player has moved
+    /// if not se the immediate angle as the old angle then
+    /// Check if the angle has increase over X degrees
+    /// 
+    ///     if so recalculate the feet
+    ///     if not nothing happens and se the current angle as old angle
+    /// 
+    /// if player has walked dont check
+    /// 
+    /// </summary>
+    private void postitioningRotationAdjustament(float angleToChange, Transform obj)
+    {
+        //check if player is moving, 1.5 to take into account the drag to slow speed down
+        //also check if the boolean for new rotation is false so that it doesnt trigger every frame
+        if (!Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.S) && !Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D))
+        {
+            if (!newRotation)
+            {
+                newRotation = true;
+                oldAngle = transform.rotation.eulerAngles.y;
+            }
+            //get the current angle of the game object
+            float currentAngle = obj.rotation.eulerAngles.y;
+            //get the absolute subtraction between the two angles to check later if the player has moved turned enough
+            float finalAngle = Mathf.Abs(currentAngle - oldAngle);
+
+            if (finalAngle >= angleToChange)
+            {
+                //set the old angle as the current one
+                oldAngle = currentAngle;
+
+
+                //call booleant to recalculate the feet position
+                recalcualte = true;
+            }
+            else
+            {
+                //if has not moved enough just keep the variable false
+                recalcualte = false;
+            }
         }
         else
         {
-            //oldPosition = nextPosition;
+            newRotation = false;
         }
-
-        
     }
+    
 
-
-    private void hipMovement()
+    private void hipMovement(float hipAnimationYValue)
     {
 
         //float newY = Mathf.Lerp(0, walkHipAnimationY, lerp);
-        float newY = Mathf.Sin(lerp * Mathf.PI) * hipAnimationModifier;
+        float newY = Mathf.Sin(lerp * Mathf.PI) * hipAnimationYValue;
         hipBone.data.position = new Vector3(0, -newY, 0);
     }
 
